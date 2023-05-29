156035a5d51cbfe86be6cd1217550aa5c3ea87d8
ABU_MUWAH <abu.muwah@gmail.com>
2023-05-29 10:28:02 +0430

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// console.log(calcAge(2000))
+
+// function calcAge(birthYear) {
+//     const age = 2023 - birthYear
+//     return age
+// }
+
+// var a = 12
+// console.log(a)
+
+// let age = 2037 - 1991
+// console.log(age)
+
+function calcAge(birthYear) {
+    const age = 2023 - birthYear
+    return age
+}
+
+console.log(calcAge(2000))
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: { 
+      center: true,
+      screens: {
+        'xl': '1220px',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
